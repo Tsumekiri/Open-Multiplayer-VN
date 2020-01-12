98669ac7535ccc55844dab4a83bf933b04c009cf
Yohaulticetl <yohaulticetl@outlook.com>
2020-01-12 20:13:57 -0300

    Fixed tab select order

--- Scripts/Scenes/Connection/MainMenu/MainMenu.gd
+++ Scripts/Scenes/Connection/MainMenu/MainMenu.gd
@@ -5,6 +5,7 @@ func _ready():
 	get_tree().set_current_scene(self)
 	$Create.connect("switch_scene", self, "switch_scene")
 	$Login.connect("switch_scene", self, "switch_scene")
+	$Exit.connect("button_up", get_tree(), "quit")
 
 func switch_scene(scene: String):
 	if (scene):
