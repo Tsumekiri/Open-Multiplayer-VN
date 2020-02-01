72b721c4c6fa5e7e5bbc6174dda52c5bd74daeb0
Yohaulticetl <yohaulticetl@outlook.com>
2020-02-01 07:49:49 -0300

    TopBar OptionButton workaround

--- Scripts/Scenes/Util/TopBar.gd
+++ Scripts/Scenes/Util/TopBar.gd
@@ -1,5 +1,18 @@
 extends Control
 
+func _ready():
+	for child in $DropdownList/Children.get_children():
+		if child is OptionButton:
+			child.connect("pressed", self, "mouse_entered_area")
+			child.get_popup().connect("mouse_entered", self, "mouse_entered_area")
+
+func has_dropdown_open() -> bool:
+	for child in $DropdownList/Children.get_children():
+		if child is OptionButton:
+			if child.is_pressed():
+				return true
+	return false
+
 func mouse_entered_area():
 	$Tween.interpolate_property($DropdownList/Children,
 		"rect_position",
@@ -8,8 +21,9 @@ func mouse_entered_area():
 	$Tween.start()
 
 func mouse_exited_area():
-	$Tween.interpolate_property($DropdownList/Children,
-		"rect_position",
-		$DropdownList/Children.get_position(),
-		Vector2(121.3, 0), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
+	if not has_dropdown_open():
+		$Tween.interpolate_property($DropdownList/Children,
+			"rect_position",
+			$DropdownList/Children.get_position(),
+			Vector2(121.3, 0), 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
