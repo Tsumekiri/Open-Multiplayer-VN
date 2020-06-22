f8090ef4550fb47b5bf151278f534543eedd4727
Yohaulticetl <yohaulticetl@outlook.com>
2020-06-22 14:46:40 -0300

    Fixed audio playback when already playing selected audio

--- Scripts/Scenes/Game/AudioPlayer.gd
+++ Scripts/Scenes/Game/AudioPlayer.gd
@@ -9,30 +9,37 @@ func receive_message(data) -> void:
 	if not data is Dictionary:
 		return
 	
-	if audio_type == "BGM" and data.bgm is Dictionary and load_bgm(data.bgm):
-		self.play()
+	if audio_type == "BGM" and data.bgm is Dictionary:
+		load_bgm(data.bgm)
 	
-	elif audio_type == "SFX" and data.sfx is Dictionary and load_sfx(data.sfx):
-		self.play()
+	elif audio_type == "SFX" and data.sfx is Dictionary:
+		load_sfx(data.sfx)
 
-func load_bgm(data: Dictionary) -> bool:
+func load_bgm(data: Dictionary) -> void:
 	if not "bgm_set" in data or not "bgm" in data:
-		return false
+		return
 	
 	var bgm_set = BGMManager.get_bgm_set(data.bgm_set)
 	if bgm_set and bgm_set is VNAudioSet:
 		var audio_stream = bgm_set.get_vn_audio(data.bgm)
-		if audio_stream:
+		if audio_stream and !self.get_stream() or (audio_stream != self.get_stream() or !self.is_playing()):
 			self.set_stream(audio_stream)
-	return true
+			self.play()
+	
+	if (data.bgm_set.empty() or data.bgm_set == "None" or data.bgm.empty() or data.bgm == "None"):
+		self.stop()
+	
 
-func load_sfx(data: Dictionary) -> bool:
+func load_sfx(data: Dictionary) -> void:
 	if not "sfx_set" in data or not "sfx" in data:
-		return false
+		return
 	
 	var sfx_set = SFXManager.get_sfx_set(data.sfx_set)
 	if sfx_set and sfx_set is VNAudioSet:
 		var audio_stream = sfx_set.get_vn_audio(data.sfx)
-		if audio_stream:
+		if audio_stream and !self.get_stream() or (audio_stream != self.get_stream() or !self.is_playing()):
 			self.set_stream(audio_stream)
-	return true
+			self.play()
+	
+	if (data.sfx_set.empty() or data.sfx_set == "None" or data.sfx.empty() or data.sfx == "None"):
+		self.stop()
