52faf0f5c3ce110a67e4d692ec01de6149bbabbf
Yohaulticetl <yohaulticetl@outlook.com>
2020-06-24 03:42:29 -0300

    Fixed background issues in conversation

--- Scripts/Scenes/Game/VNAnimatedSprite.gd
+++ Scripts/Scenes/Game/VNAnimatedSprite.gd
@@ -5,6 +5,9 @@ export(String) var sprite_position = ""
 
 const MAX_HEIGHT: float = 600.0 # Change this if you change the target resolution on your project settings
 
+var _last_sprite_set
+var _last_sprite
+
 func _ready():
 	MessageManager.connect("message_received", self, "receive_message")
 
@@ -14,32 +17,67 @@ func receive_message(data) -> void:
 		return
 	
 	hide()
+
 	if sprite_position:
-		check_characters(data)
-	else:
-		if data.background.is_video == "false":
+		var character = check_characters(data)
+
+		if character and not (character.name == _last_sprite_set and character.expression == _last_sprite):
+			setup_character(character.name, character.expression)
+		elif character:
+			if not self.playing:
+				play(character.expression)
+			show()
+
+		if character:
+			_last_sprite_set = character.name
+			_last_sprite = character.expression
+		else:
+			_last_sprite_set = null
+			_last_sprite = null
+	elif "background_set" in data.background and "background" in data.background and data.background.is_video == "false":
+		if not (data.background.background_set == _last_sprite_set and data.background.background == _last_sprite):
+			if is_no_background(data.background) and self.playing:
+				stop()
+			
 			check_background(data.background)
+			
+			_last_sprite_set = data.background.background_set
+			_last_sprite = data.background.background
+		else:
+			if not self.playing:
+				play(data.background.background)
+			show()
+		
+
+# Checks if background should be removed
+func is_no_background(data_background: Dictionary):
+	return data_background.background or data_background.background == "None" or not data_background.background_set or data_background.background_set == "None"
 
 # Checks whether background exists and sets it up
-func check_background(data: Dictionary) -> void:
+func check_background(data: Dictionary) -> bool:
 	var target = BackgroundManager.get_background_set(data.background_set)
 	if target:
-		setup_background(target, data.background)
+		return setup_background(target, data.background)
+	elif self.playing:
+		stop()
+	return false
 
 # Sets background, fits it to screen and plays animation
-func setup_background(background_set: BackgroundSet, background: String) -> void:
+func setup_background(background_set: BackgroundSet, background: String) -> bool:
 	set_sprite_frames(background_set.get_sprite_frames())
 	if frames.has_animation(background):
 		fit_sprite(background)
 		play(background)
 		show()
+		return true
+	return false
 
 # Is a character sprite, checks received characters in message and sets it up if
 # the position is right
-func check_characters(data: Dictionary) -> void:
+func check_characters(data: Dictionary):
 	for character in data.characters:
 		if character.position == sprite_position and character.name:
-			setup_character(character.name, character.expression)
+			return character
 
 # Sets character, fits to screen and plays animation
 func setup_character(character: String, expression: String) -> void:
--- Scripts/Scenes/Game/VideoPlayer.gd
+++ Scripts/Scenes/Game/VideoPlayer.gd
@@ -2,18 +2,35 @@ extends VideoPlayer
 
 const MAX_HEIGHT: float = 600.0 # Change this if you change the target resolution on your project settings
 
+var _last_bg_set
+var _last_bg
+
 func _ready():
 	MessageManager.connect("message_received", self, "receive_message")
 
 # Decides what to do after the sprite receives a message
 func receive_message(data) -> void:
-	if not data is Dictionary:
+	if not data is Dictionary or not data.background is Dictionary:
+		return
+
+	if data.background.background_set == _last_bg_set and data.background.background == _last_bg:
 		return
 	
 	hide()
 	if data.background.is_video == "true":
-		check_video(data.background)
-		show()
+		if is_no_background(data.background) and is_playing():
+			stop()
+		else:
+			check_video(data.background)
+	elif is_playing():
+		stop()
+	
+	_last_bg_set = data.background.background_set
+	_last_bg = data.background.background
+
+# Checks if background should be removed
+func is_no_background(data_background: Dictionary):
+	return data_background.background or data_background.background == "None" or not data_background.background_set or data_background.background_set == "None"
 
 # Checks whether background exists and sets it up
 func check_video(data: Dictionary) -> void:
@@ -24,6 +41,7 @@ func check_video(data: Dictionary) -> void:
 # Sets background, fits it to screen and plays animation
 func setup_video(background_set: VideoSet, video_name: String) -> void:
 	var video = background_set.get_vn_video(video_name)
-	if video and not (self.get_stream() and self.get_stream().get_file() == video.get_video().get_file() and self.is_playing()):
-		self.set_stream(video.get_video())
-		self.play()
+	if video and not (get_stream() and get_stream().get_file() == video.get_video().get_file() and is_playing()):
+		set_stream(video.get_video())
+		play()
+		show()
